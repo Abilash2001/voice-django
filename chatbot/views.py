c3a3b3d4966a9533dac3c4b77e1d418e2af66025
ATOM_MAX <52504909+Abilash2001@users.noreply.github.com>
2022-10-07 23:44:40 +0530

    +

--- chatbot/views.py
+++ chatbot/views.py
@@ -10,8 +10,7 @@ class SearchEngine:
     qresponse = {
         ("hi","hello") : ("Hey, Thanks for visiting Voizfonica, How may I help you?","hello"),
         ("prepaid connection activated","postpaid connection activated","connection activated"): ("After you receive a new prepaid/postpaid SIM, it usually takes a few hours to get activated. Once your SIM is activated, you can enjoy all the benefits associated with your prepaid/postpaid plan.","testing")
-        }
-    
+    }
     def __init__(self) -> None:
         pass
     def find(self,message: str) -> str:
@@ -22,6 +21,6 @@ def getResponse(request):
     if(request.method == "POST"):
         userMessage = request.POST.get('newchat')
         search = SearchEngine()
-        return HttpResponse(search.find(userMessage))
+        return HttpResponse(userMessage)
     else:
         return HttpResponse("INVALID REQUEST")
